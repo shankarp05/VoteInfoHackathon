47526619118c07a739cdac05d480d6d25b420711
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-11-10 01:00:06 -0500

    adding functionality to chat about higher members of congress

--- islands/StartingPrompts.tsx
+++ islands/StartingPrompts.tsx
@@ -2,12 +2,19 @@
 import { Button } from "../components/Button.tsx";
 
 export default function ButtonIsland({ onClick }: { onClick: (text: string) => void }) {
-  const buttonText = "What are Jake Ashby's Views on IVF?"; // The text to populate the chatbox with
+  const buttonText1 = "What is Jake Ashby's stance on IVF?"; // The text to populate the chatbox with
+  const buttonText2 = "What are Juan Ardila's views on landlord-tenant relations?"; // The text to populate the chatbox with
+  const buttonText3 = "How has Simcha Felder worked to ease the financial burden on property owners in New York City?"; // The text to populate the chatbox with
 
   return (
-    <Button
-      text={buttonText}
-      onClick={() => onClick(buttonText)}
-    />
+  <div>
+  
+  <Button text={buttonText1} onClick={() => onClick(buttonText1)} />
+      <Button text={buttonText2} onClick={() => onClick(buttonText2)} />
+      <Button text={buttonText3} onClick={() => onClick(buttonText3)} />
+
+
+      </div>
+    
   );
 }
